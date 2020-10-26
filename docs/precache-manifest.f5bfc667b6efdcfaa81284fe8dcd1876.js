60bbbfca45395c8de69227b5434df6c3400a21ea
clizanocrc <kaycrc@gmail.com>
2020-10-26 11:03:32 -0600

    docs agregado

---
+++ docs/precache-manifest.f5bfc667b6efdcfaa81284fe8dcd1876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6d8f5f5606f2074a86142a3e43ec02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69aa043de68b4e295cb",
+    "url": "/static/css/main.612a309e.chunk.css"
+  },
+  {
+    "revision": "17511b471bfdde6e14c9",
+    "url": "/static/js/2.ee48284c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.ee48284c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69aa043de68b4e295cb",
+    "url": "/static/js/main.da859ce9.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
